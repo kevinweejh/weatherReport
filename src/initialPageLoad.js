927b61c5f5a1653c50c45633705656a5da7538f9
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-08 15:30:02 +0800

    chore: remove unnecessary template files

--- src/initialPageLoad.js
+++
@@ -1,6 +0,0 @@
-export default () => {
-    const container = document.getElementById('content');
-    container.classList.add("bg-[url('./restaurant.jpg')]", 'h-screen', 'flex', 'flex-col');
-
-    const tabBar = document.createElement('div');
-}
