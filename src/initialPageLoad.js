098193e1835c2cb2ac1b9096992590c3c7cc944b
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-08 15:31:09 +0800

    chore: restore initialPageLoad.js

---
+++ src/initialPageLoad.js
@@ -0,0 +1,6 @@
+export default () => {
+    const container = document.getElementById('content');
+    container.classList.add("bg-[url('./restaurant.jpg')]", 'h-screen', 'flex', 'flex-col');
+
+    const tabBar = document.createElement('div');
+}
