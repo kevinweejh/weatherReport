6fb6200b61a241cd94ef50210c2807e4a6bdcc7e
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-09 15:32:59 +0800

    build: set project title

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Restaurant de la Victoire',
+      title: 'Weather Report',
       template: './src/template.html'
     }),
   ],
