1d226c4c6326117dc7d69fb0ff21935dc25c118b
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-09 14:25:05 +0800

    style: layout for weather report dialog

--- src/displayWeatherInfo.js
+++ src/displayWeatherInfo.js
@@ -14,13 +14,13 @@ export default (weatherInfo) => {
     const temp = createElement('p', ['text-base', 'font-normal', 'my-5'], `Temperature: ${temp_c}°C`);
     const cond = createElement('p', ['text-base', 'font-normal', 'my-5'], `Condition:   ${condition}`);
 
-    const celsiusBtn = createElement('input', [], '°C');
+    const celsiusBtn = createElement('input', ['m-2', 'z-10'], '°C');
     celsiusBtn.setAttribute('name', 'unitOption');
     celsiusBtn.setAttribute('value', '°C');
     celsiusBtn.setAttribute('type', 'radio');
     celsiusBtn.setAttribute('checked', '');
 
-    const fahrenheitBtn = createElement('input', [], '°F');
+    const fahrenheitBtn = createElement('input', ['m-2', 'z-10'], '°F');
     fahrenheitBtn.setAttribute('name', 'unitOption');
     fahrenheitBtn.setAttribute('value', '°F');
     fahrenheitBtn.setAttribute('type', 'radio');
@@ -28,13 +28,16 @@ export default (weatherInfo) => {
     const closeBtn = createElement('btn', ['w-full', 'p-2.5', 'my-2.5', 'border', 'rounded-2xl', 'text-white', 'bg-blue-600', 'hover:bg-blue-500', 'hover:text-gray-200', 'cursor-pointer'], 'Done');
     closeBtn.addEventListener('click', () => weatherReport.close());
     
-    appendMultipleChildren(formReport, temp, celsiusBtn, fahrenheitBtn, cond, closeBtn)
-    weatherReport.appendChild(formReport); 
-    const container = document.querySelector("body");2
+    const btnDiv = createElement('div', ['flex', 'justify-center', 'relative'], '');
+    const btnLabels = createElement('pre', ['text-center', 'absolute', 'whitespace-pre', 'font-sans'], '°C              °F');
+
+    appendMultipleChildren(btnDiv, celsiusBtn, fahrenheitBtn, btnLabels);
+    appendMultipleChildren(formReport, temp, btnDiv, cond, closeBtn)
+    weatherReport.appendChild(formReport);  
+    const container = document.querySelector("body");
     container.appendChild(weatherReport);
 
     const radioButtons = document.querySelectorAll('input[name="unitOption"]');
-    console.log(radioButtons);
     
     radioButtons.forEach((radio) => {
         radio.addEventListener('change', () => {
