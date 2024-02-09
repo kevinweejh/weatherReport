7642cceb2b7708ac662b8170c366d89b7ed58a83
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-09 13:11:53 +0800

    feat: show weather info on dialog

--- src/displayWeatherInfo.js
+++ src/displayWeatherInfo.js
@@ -1,7 +1,40 @@
+import { createElement, appendMultipleChildren } from "./utils";
+
 export default (weatherInfo) => {
     const [temp_c, temp_f, condition] = weatherInfo;
 
-    alert(`temp: ${temp_c} degC`);
-    alert(`temp: ${temp_f} degF`);
-    alert(`condition: ${condition}`);
+    const weatherReport = createElement('dialog', [], '', 'weatherReport');
+    const temp = createElement('p', [], '');
+    const cond = createElement('p', [], '');
+
+    const celsiusBtn = createElement('input', [], '°C');
+    celsiusBtn.setAttribute('name', 'unitOption');
+    celsiusBtn.setAttribute('value', '°C');
+    celsiusBtn.setAttribute('type', 'radio');
+
+    const fahrenheitBtn = createElement('input', [], '°F');
+    fahrenheitBtn.setAttribute('name', 'unitOption');
+    fahrenheitBtn.setAttribute('value', '°F');
+    fahrenheitBtn.setAttribute('type', 'radio');
+    
+    appendMultipleChildren(weatherReport, temp, celsiusBtn, fahrenheitBtn, cond); 
+    const container = document.querySelector("body");
+    container.appendChild(weatherReport);
+
+    const radioButtons = document.querySelectorAll('input[name="unitOption"]');
+    console.log(radioButtons);
+    radioButtons.forEach((radio) => {
+        radio.addEventListener('change', () => {
+            if (radio.value == '°C') {
+                temp.innerText = `${temp_c} ${radio.value}`;
+                cond.innerText = condition;
+            }
+            if (radio.value == '°F') {
+                temp.innerText = `${temp_f} ${radio.value}`;
+                cond.innerText = condition;
+            }
+        })
+    })
+    
+    weatherReport.showModal();
 }
