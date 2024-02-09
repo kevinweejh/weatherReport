34f58a34ea74314f029dffae11d20552ceafd9ed
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-09 13:59:02 +0800

    feat: properly display temp and condition

--- src/displayWeatherInfo.js
+++ src/displayWeatherInfo.js
@@ -3,38 +3,46 @@ import { createElement, appendMultipleChildren } from "./utils";
 export default (weatherInfo) => {
     const [temp_c, temp_f, condition] = weatherInfo;
 
-    const weatherReport = createElement('dialog', [], '', 'weatherReport');
-    const temp = createElement('p', [], '');
-    const cond = createElement('p', [], '');
+    const weatherReport = createElement('dialog', ['border', 'border-gray-500', 'rounded-2xl', 'p-5', 'w-96', 'm-auto'], '', 'weatherReport');
+    const formReport = createElement(
+        "form",
+        ['text-lg', 'font-bold', 'mb-5', 'flex', 'flex-col', 'text-center'],
+        "Weather Report"
+      );
+    formReport.setAttribute('method', 'dialog');
+
+    const temp = createElement('p', ['text-base', 'font-normal', 'my-5'], `Temperature: ${temp_c}°C`);
+    const cond = createElement('p', ['text-base', 'font-normal', 'my-5'], `Condition:   ${condition}`);
 
     const celsiusBtn = createElement('input', [], '°C');
     celsiusBtn.setAttribute('name', 'unitOption');
     celsiusBtn.setAttribute('value', '°C');
     celsiusBtn.setAttribute('type', 'radio');
+    celsiusBtn.setAttribute('checked', '');
 
     const fahrenheitBtn = createElement('input', [], '°F');
     fahrenheitBtn.setAttribute('name', 'unitOption');
     fahrenheitBtn.setAttribute('value', '°F');
     fahrenheitBtn.setAttribute('type', 'radio');
 
-    const closeBtn = createElement('btn', [], 'Done');
+    const closeBtn = createElement('btn', ['w-full', 'p-2.5', 'my-2.5', 'border', 'rounded-2xl', 'text-white', 'bg-blue-600', 'hover:bg-blue-500', 'hover:text-gray-200', 'cursor-pointer'], 'Done');
     closeBtn.addEventListener('click', () => weatherReport.close());
     
-    appendMultipleChildren(weatherReport, temp, celsiusBtn, fahrenheitBtn, cond, closeBtn); 
-    const container = document.querySelector("body");
+    appendMultipleChildren(formReport, temp, celsiusBtn, fahrenheitBtn, cond, closeBtn)
+    weatherReport.appendChild(formReport); 
+    const container = document.querySelector("body");2
     container.appendChild(weatherReport);
 
     const radioButtons = document.querySelectorAll('input[name="unitOption"]');
     console.log(radioButtons);
+    
     radioButtons.forEach((radio) => {
         radio.addEventListener('change', () => {
             if (radio.value == '°C') {
-                temp.innerText = `${temp_c} ${radio.value}`;
-                cond.innerText = condition;
+                temp.innerText = `Temperature: ${temp_c} ${radio.value}`;
             }
             if (radio.value == '°F') {
-                temp.innerText = `${temp_f} ${radio.value}`;
-                cond.innerText = condition;
+                temp.innerText = `Temperature: ${temp_f} ${radio.value}`;
             }
         })
     })
