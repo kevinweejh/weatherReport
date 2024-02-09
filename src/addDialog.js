b615e74d818b3bdc89142ac46b4caa07b71889f3
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-09 13:34:14 +0800

    style: improve styling for location selector dialog

--- src/addDialog.js
+++ src/addDialog.js
@@ -7,33 +7,50 @@ export default () => {
 
   const dialog = createElement(
     "dialog",
-    [],
+    ['border', 'border-gray-500', 'rounded-2xl', 'p-5', 'w-96', 'm-auto'],
     "", 'dialogBox'
   );
 
   const form = createElement(
     "form",
-    [],
-    "", 'formElement'
+    ['text-lg', 'font-bold', 'mb-5', 'flex', 'flex-col', 'text-center'],
+    "Location Selector", 'formElement'
   );
   form.setAttribute('method', 'dialog');
 
   const countrySelector = createElement(
     "select", 
-    [],
+    ['w-full', 'p-2.5', 'mb-2.5', 'border', 'rounded', 'border-blue-200'],
     "", 'countrySelector'
   )
+  const countryPlaceholder = createElement(
+    "option",
+    [],
+    "Select country"
+  )
+  countryPlaceholder.setAttribute("value", "");
+  countryPlaceholder.setAttribute("disabled", "");
+  countryPlaceholder.setAttribute("selected", "");
+  countrySelector.appendChild(countryPlaceholder);
   
   const citySelector = createElement(
     "select", 
-    [],
+    ['w-full', 'p-2.5', 'mb-2.5', 'border', 'rounded', 'border-blue-200'],
     "", 'citySelector'
   )
-  citySelector.setAttribute('disabled', '');
+  const cityPlaceholder = createElement(
+    "option",
+    [],
+    "Select City"
+  )
+  cityPlaceholder.setAttribute("value", "");
+  cityPlaceholder.setAttribute("disabled", "");
+  cityPlaceholder.setAttribute("selected", "");
+  citySelector.appendChild(cityPlaceholder);
 
   const submitBtn = createElement(
     "btn", 
-    [],
+    ['w-full', 'p-2.5', 'my-2.5', 'border', 'rounded-2xl', 'text-white', 'bg-blue-200', 'hover:bg-white', 'hover:text-blue-200', 'cursor-pointer'],
     "Submit",
     'submitBtn'
   )
